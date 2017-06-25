c8643f9fec786296949b9b3235e8da17400fc39d
Takashi Kawano <kawano.fanta@gmail.com>
2017-06-25 21:18:42 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+enchant();
+
+window.onload = function() {
+    var core = new Core(320, 130);
+    core.preload('chara1.png');
+    core.fps = 15;
+    core.onload = function() {
+        var label = new Label();
+        label.x = 280;
+        label.y = 20;
+        label.color = 'black';
+        label.font = '14pz "Arial"';
+        label.text = '0';
+        label.on('enterframe', function() {
+            label.text = (core.frame / core.fps).toFixed(2);
+        });
+
+        var Bear = Class.create(Sprite, {
+            initialize: function(x, y) {
+                Sprite.call(this, 32, 32);
+                this.x = x;
+                this.y = y;
+                this.frame = rand(5);
+                this.opacity = rand(100) / 10;
+                this.image = core.assets['chara1.png'];
+                this.on('enterframe', function() {
+                    this.tl.moveBy(rand(100), 0, 40, enchant.Easing.BOUNCE_EASEOUT)
+                       .moveBy(-rand(100), -rand(20), rand(20))
+                       .fadeOut(20)
+                       .fadeIn(10)
+                       .loop();
+                });
+                core.rootScene.addChild(this);
+            }
+        });
+
+        var bears = [];
+        for (var i=0; i < 100; i++) {
+            bears[i] = new Bear(rand(320), rand(320));
+        }
+
+        core.rootScene.addChild(label);
+    };
+    core.start();
+
+    function rand(n) {
+        return Math.floor(Math.random() * (n + 1));
+    }
+};
+
